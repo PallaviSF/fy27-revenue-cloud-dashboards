728311a6ec17bdc3a1a6be9ea5430854681c53d5
PallaviSF <pallavi.pandey@salesforce.com>
2026-07-17 04:13:38 +0530

    Trigger Pages rebuild for sage green theme

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-self.addEventListener("fetch",function(e){if(e.request.mode==="navigate"){e.respondWith(fetch(e.request,{cache:"no-store"}));}});
+self.addEventListener("fetch",function(e){if(e.request.mode==="navigate"){e.respondWith(fetch(e.request,{cache:"no-store"}));}});// sage-v3
